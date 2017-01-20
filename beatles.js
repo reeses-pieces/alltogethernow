155c2f8da48a224d1cb83f62f85071f50d51d55c
reeses-pieces <nicholasreis@icloud.com>
2017-01-19 17:11:12 -0800

    add trim and lowercase to speech transcript function

--- beatles.js
+++ beatles.js
@@ -11,17 +11,12 @@ const recognition = new SpeechRecognition();
 recognition.interimResults = true;
 
 const keyWords = {
-  "1": {top: 0, right: 385, bottom: 381, left: 0},
-  "2": {top: 0, right: 827, bottom: 381, left: 385},
-  "3": {top: 382, right: 385, bottom: 762, left: 0},
-  "4": {top: 382, right: 800, bottom: 762, left: 385}
+  "1": changeImgCrop.bind(null, {top: 0, right: 385, bottom: 381, left: 0}),
+  "2": changeImgCrop.bind(null, {top: 0, right: 827, bottom: 381, left: 385}),
+  "3": changeImgCrop.bind(null, {top: 382, right: 385, bottom: 762, left: 0}),
+  "4": changeImgCrop.bind(null, {top: 382, right: 800, bottom: 762, left: 385})
 };
 
-// 1st - rect(0px,385px,385px,0px)
-// 2nd - rect(0px,827px,385px,384px)
-// 3rd - rect(382px,385px,762px,0px)
-// 4th - rect(382px,800px,762px,385px)
- 
 function changeImgCrop(args = {}) {
   $('#portrait').css('clip', `rect(${args.top}px,${args.right}px,${args.bottom}px,${args.left}px)`);
 }
@@ -29,7 +24,9 @@ function changeImgCrop(args = {}) {
 function speechTranscript(e) {
   const transcript = Array.from(e.results)
     .map(result => result[0])
-    .map(result => result.transcript).join('');
+    .map(result => result.transcript).join('')
+    .trim()
+    .toLowerCase();
     console.log(transcript);
 
     processCommand(transcript);
