f8a598e05c0f477571d58745d3df97fc74382e01
reeses-pieces <nicholasreis@icloud.com>
2017-01-19 17:58:52 -0800

    add a little more function

--- beatles.js
+++ beatles.js
@@ -10,15 +10,26 @@ const recognition = new SpeechRecognition();
 // Live visual feedback to show what you are speaking
 recognition.interimResults = true;
 
+const mainContent = $("#main-container");
+
 const keyWords = {
   "1": changeImgCrop.bind(null, {top: 0, right: 385, bottom: 381, left: 0}),
   "2": changeImgCrop.bind(null, {top: 0, right: 827, bottom: 381, left: 385}),
   "3": changeImgCrop.bind(null, {top: 382, right: 385, bottom: 762, left: 0}),
-  "4": changeImgCrop.bind(null, {top: 382, right: 800, bottom: 762, left: 385})
+  "4": changeImgCrop.bind(null, {top: 382, right: 800, bottom: 762, left: 385}, true),
+  "": aLittleMore
 };
 
-function changeImgCrop(args = {}) {
+function changeImgCrop(args={}, lastImage=false) {
   $('#portrait').css('clip', `rect(${args.top}px,${args.right}px,${args.bottom}px,${args.left}px)`);
+  if(lastImage) {
+    setTimeout(aLittleMore, 1000);
+  }
+}
+
+function aLittleMore() {
+  // $("#portrait").addClass("hidden");
+  mainContent.html("<p>Can I have a little more?</p>");
 }
 
 function speechTranscript(e) {
